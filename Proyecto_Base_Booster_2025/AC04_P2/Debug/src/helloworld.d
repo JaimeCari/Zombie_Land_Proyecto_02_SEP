a07986094fd30dc9fdae98a799706258dee5c4a4
Jaime González <jagonzalez33@uc.cl>
2026-05-09 22:56:02 -0400

    First push

---
+++ Proyecto_Base_Booster_2025/AC04_P2/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/DEMO_wrapper/export/DEMO_wrapper/sw/DEMO_wrapper/domain_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
---
+++ Proyecto_Base_Booster_2025/Proyecto_Base_Booster/test_boot_ac04/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xil_types.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xparameters_ps.h \
+ C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+C:/Users/aleja/OneDrive/Escritorio/UC/SEP/Proyecto_Base_Booster_2025/Proyecto_Base_Booster/AC04_P2/export/AC04_P2/sw/AC04_P2/standalone_domain/bspinclude/include/bspconfig.h:
